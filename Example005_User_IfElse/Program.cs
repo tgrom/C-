5810464e1c2869165017b7a6a02ac0262641d542
tgrom <tg.romanchenko@gmail.com>
2022-07-14 23:11:54 +0300

    User If and Else

---
+++ Example005_User_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это МАША");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
