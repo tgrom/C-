a62bdd009bf6131278be0113106193c7d54a519f
tgrom <tg.romanchenko@gmail.com>
2022-07-14 22:52:54 +0300

    add random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int number_1 = 5;
-int number_2 = 7;
+﻿int number_1 = new Random().Next(1, 10);
+Console.WriteLine(number_1);
+int number_2 = new Random().Next(1, 10);
+Console.WriteLine(number_2);
 int result = number_1 + number_2;
 Console.WriteLine(result);
